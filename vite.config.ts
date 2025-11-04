5c821abf7b2b4ec56912b6626a14e5bd3e50f136
yishayweb <yishayweb@gmail.com>
2025-11-04 13:35:47 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,34 @@ import react from "@vitejs/plugin-react";
 import { federation } from "@module-federation/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    port: 4000,
-    origin: "http://localhost:4000",
-  },
-  preview: {
-    port: 4000,
-  },
-  base: mode === "development" ? "/" : "http://localhost:4000/",
-  plugins: [
-    react(),
-    federation({
-      name: "host",
-      remotes: {
-        oneRemote: {
-          type: "module",
-          name: "oneRemote",
-          entry: "http://localhost:4001/remoteEntry.js",
-          entryGlobalName: "oneRemote",
-          shareScope: "default",
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+  return {
+    server: {
+      port: 4000,
+      origin: "http://localhost:4000",
+    },
+    preview: {
+      port: 4000,
+    },
+    // base: mode === "development" ? "/" : "http://localhost:4000/",
+    plugins: [
+      react(),
+      federation({
+        name: "host",
+        remotes: {
+          oneRemote: {
+            type: "module",
+            name: "oneRemote",
+            // entry: "http://localhost:4001/remoteEntry.js",
+            entry: "https://remote-mf-tau.vercel.app/remoteEntry.js",
+            entryGlobalName: "oneRemote",
+            shareScope: "default",
+          },
         },
-      },
-      filename: "remoteEntry.js",
-      shared: ["react", "react-dom", "react-router-dom"],
-    }),
-  ],
-}));
+        filename: "remoteEntry.js",
+        shared: ["react", "react-dom", "react-router-dom"],
+      }),
+    ],
+  };
+});
